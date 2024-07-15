12750f067fe35d7c493cc934d784de422a60f94b
ChangkiY <53373279+changkiyun@users.noreply.github.com>
2024-07-15 12:31:46 +0900

    @Transactional 적용

--- BackEnd/goorm/src/main/java/backend/goorm/chat/controller/ChatController.java
+++ BackEnd/goorm/src/main/java/backend/goorm/chat/controller/ChatController.java
@@ -7,17 +7,20 @@ import backend.goorm.chat.model.response.ChatResponse;
 import backend.goorm.chat.repository.ChatRepository;
 import backend.goorm.chat.repository.ChatRoomRepository;
 import backend.goorm.chat.service.ChatService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
+@Transactional
 public class ChatController {
     private final ChatService chatService;
     private final ChatRepository chatRepository;
